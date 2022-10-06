cf4513cec4baaff43a132c2e727b2d891ead393f
Sergey <gw93U@yandex.ru>
2022-10-06 20:28:30 +0500

    Homework 6

---
+++ Program 41/Program.cs
@@ -0,0 +1,48 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int InputNumber(string text) // Ручной ввод числа
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void InputArray(int[] array) // Ручной ввод массива чисел
+{
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = InputNumber($"Введите {i + 1}-е число: ");
+    }
+}
+
+string StringArray(int[] array, string split) // Преобразование массива в строку с заданным разделителем
+{
+    int length = array.Length; // получение длины массива
+    string textarray = String.Empty; // обнуление строки
+    for (int i = 0; i < length; i++) // цикл по всем элементам массива
+    {
+        textarray += array[i]; // добавление в строку значение текущего элемента массива
+        if (i < length - 1) // проверка является ли элемент последним (нужно ли добавлять разделитель в строку)
+            textarray += split; // добавление разделителя между элементами в строку
+    }
+    return textarray; // возврат значения элементов массива в виде полученной строки
+}
+
+int LimitedSum(int[] array, int min, int max) // Суммирование элементов массива в заданном промежутке значений
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= min && array[i] <= max)
+            sum += array[i];
+    }
+    return sum;
+}
+
+int n = InputNumber("Введите количество вводимых чисел: ");
+int min = 1,
+    max = 2147483647;
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine("[" + StringArray(array, ", ") + "]");
+Console.WriteLine($"Сумма чисел больше 0: {LimitedSum(array, min, max)}");
---
+++ Program 43/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int InputNumber(string text) // Ручной ввод числа
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double[] GetIntersection(double b1, double k1, double b2, double k2) // Получение точки пересечения двух прямых, заданных уравнениями
+{
+    if (k1 == k2)
+        return new double[2] { 0, 0 }; // Возвращает 0,0 если пересечения нет (как обойти?)
+    double[] intersection = new double[2];
+    intersection[0] = (b2 - b1) / (k1 - k2);
+    intersection[1] = intersection[0] * k1 + b1;
+    return intersection;
+}
+
+double b1 = InputNumber("Введите b1: "),
+    k1 = InputNumber("Введите k1: "),
+    b2 = InputNumber("Введите b2: "),
+    k2 = InputNumber("Введите k2: ");
+
+double[] cross = GetIntersection(b1, k1, b2, k2);
+Console.WriteLine(
+    $"x={cross[0]}, y={cross[1]} - точка пересечения прямых y = {k1}x + {b1}, y = {k2}x + {b2}"
+);
